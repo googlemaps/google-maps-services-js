35b45a01200f023f0994efe0436ffa502f05dbfa
Stephen Farrar <stephen.farrar@gmail.com>
2015-11-11 07:53:47 +1100

    Removed unnecessary process.nextTick in the retry logic.

--- attempt.js
+++ attempt.js
@@ -10,31 +10,29 @@ exports.inject = (setTimeout) => ({
     var startTime = +new Date;
     var cancelled = false;
 
-    (function tryItAndSee() {
-      process.nextTick(() => {
-        if (cancelled) return;
+    process.nextTick(function tryItAndSee() {
+      if (cancelled) return;
 
-        doSomething((err, result) => {
-          if (err != null) {
-            callback(err, null);
-            return;
-          }
-          if (isSuccessful(result)) {
-            callback(null, result);
-            return;
-          }
+      doSomething((err, result) => {
+        if (err != null) {
+          callback(err, null);
+          return;
+        }
+        if (isSuccessful(result)) {
+          callback(null, result);
+          return;
+        }
 
-          var waitTime = interval * (1 + jitter * (2 * Math.random() - 1));
-          interval *= increment;
-          if (+new Date + waitTime < startTime + timeout) {
-            setTimeout(tryItAndSee, waitTime);
-            return;
-          }
+        var waitTime = interval * (1 + jitter * (2 * Math.random() - 1));
+        interval *= increment;
+        if (+new Date + waitTime < startTime + timeout) {
+          setTimeout(tryItAndSee, waitTime);
+          return;
+        }
 
-          callback(new Error('timeout'), null);
-        });
+        callback(new Error('timeout'), null);
       });
-    })();
+    });
 
     return {cancel: () => {
       cancelled = true;
