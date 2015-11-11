f17756cf2a29b2bc088c864ad78b462b38c9845e
Stephen McDonald <steve@jupo.org>
2015-11-11 14:40:39 +1100

    Fix reverse geocode test

--- spec/geocode-spec.js
+++ spec/geocode-spec.js
@@ -29,14 +29,14 @@ describe('geocode client library', () => {
 
   it('reverse geocodes the coordinates for the Sydney Opera House', (done) => {
     googleMaps.reverseGeocode({
-      latlng: [-33.8674869, 151.2069902]
+      latlng: [-33.8571965, 151.2151398],
     })
     .then((response) => response.json())
     .then((json) => {
       expect(json.results).toEqual(
           jasmine.arrayContaining([
             jasmine.objectContaining({
-              place_id: 'ChIJidzEjmauEmsRwb535u6rCA4'
+              formatted_address: '2A Macquarie St, Sydney NSW 2000, Australia'
             })
           ]));
     })
