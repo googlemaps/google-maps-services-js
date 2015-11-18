a6ec684ce9081b5e2d3db391c1897fe072fc8ab3
Stephen Farrar <stephen.farrar@gmail.com>
2015-11-18 14:12:31 +1100

    Rename circular buffer methods to be more obvious. push() becomes insert(). tail() becomes item() and can access any item in the buffer.

--- spec/circular-buffer-spec.js
+++ spec/circular-buffer-spec.js
@@ -8,46 +8,68 @@ describe('CircularBuffer,', function() {
   });
 
   describe('when empty,', function() {
-    it('.tail() returns undefined', function() {
-      expect(list.tail()).toBe(undefined);
+    it('.item() returns undefined', function() {
+      expect(list.item(0)).toBe(undefined);
+      expect(list.item(1)).toBe(undefined);
+      expect(list.item(SIZE - 1)).toBe(undefined);
     });
   });
 
-  describe('when one item has been pushed,', function() {
+  describe('when one item has been inserted,', function() {
     beforeEach(function() {
-      list.push(100);
+      list.insert('item 0');
     });
-    it('.tail() returns undefined', function() {
-      expect(list.tail()).toBe(undefined);
+    it('.item(0) returns the recently-inserted item', function() {
+      expect(list.item(0)).toBe('item 0');
+    });
+    it('.item(i) returns undefined for i > 0', function() {
+      expect(list.item(1)).toBe(undefined);
+      expect(list.item(SIZE - 1)).toBe(undefined);
     });
   });
 
-  describe('when SIZE - 1 items have been pushed,', function() {
+  describe('when SIZE - 1 items have been inserted,', function() {
     beforeEach(function() {
       for (var i = 0; i < SIZE - 1; ++i) {
-        list.push(i + 100);
+        list.insert('item ' + i);
       }
     });
-    it('.tail() returns undefined', function() {
-      expect(list.tail()).toBe(undefined);
+    it('.item(i) returns items in reverse order', function() {
+      expect(list.item(0)).toBe('item ' + (SIZE - 2));
+      expect(list.item(SIZE - 2)).toBe('item 0');
+    });
+    it('.item(SIZE - 1) returns undefined', function() {
+      expect(list.item(SIZE - 1)).toBe(undefined);
+    });
+  });
+
+  describe('when SIZE items have been inserted,', function() {
+    beforeEach(function() {
+      for (var i = 0; i < SIZE; ++i) {
+        list.insert('item ' + i);
+      }
+    });
+    it('.item(i) returns items in reverse order', function() {
+      expect(list.item(0)).toBe('item ' + (SIZE - 1));
+      expect(list.item(SIZE - 1)).toBe('item 0');
+    });
+  });
+
+  describe('when SIZE + 1 items have been inserted,', function() {
+    beforeEach(function() {
+      for (var i = 0; i < SIZE + 1; ++i) {
+        list.insert('item ' + i);
+      }
+    });
+    it('.item(i) returns items in reverse order', function() {
+      expect(list.item(0)).toBe('item ' + SIZE);
+      expect(list.item(SIZE - 1)).toBe('item 1');
     });
 
-    describe('when another item is pushed,', function() {
-      beforeEach(function() {
-        list.push(SIZE + 100);
-      });
-      it('.tail() returns the first item', function() {
-        expect(list.tail()).toBe(100);
-      });
-
-      describe('when another item is pushed,', function() {
-        beforeEach(function() {
-          list.push(SIZE + 101);
-        });
-        it('.tail() returns the second item', function() {
-          expect(list.tail()).toBe(101);
-        });
-      });
+    it('"item 0" is no longer in the buffer', function() {
+      for (var i = 0; i < SIZE; ++i) {
+        expect(list.item(i)).not.toBe('item 0');
+      }
     });
   });
 });
