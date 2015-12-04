0d3d70362968901bfef95b7cd0196ddcc99006c8
Stephen Farrar <stephen.farrar@gmail.com>
2015-12-04 11:53:46 +1100

    Add tests for all the directions API options. Fix problem with convert.timeStamp(). Add extra checks for invalid combinations of options.

--- e2e/directions-spec.js
+++ e2e/directions-spec.js
@@ -4,12 +4,24 @@ var objectContaining = jasmine.objectContaining;
 describe('directions client library', function() {
   var googleMaps = require('./service');
 
+  var inOneHour = new Date().getTime() + 60 * 60 * 1000;
+
+  function expectOK(response) {
+    // if (response.status !== 200) {
+    //   console.log(JSON.stringify(response, null, 2));
+    // }
+    expect(response.status).toBe(200);
+    expect(response.json.status).toBe('OK');
+    return response;
+  }
+
   it('gets the directions from the Sydney Town Hall to Parramatta, NSW', function(done) {
     googleMaps.directions({
-      origin: 'Sydney Town Hall',
+      origin: 'Town Hall, Sydney, NSW',
       destination: 'Parramatta, NSW',
     })
     .asPromise()
+    .then(expectOK)
     .then(function(response) {
       expect(response.json.routes).toEqual(
           arrayContaining([
@@ -22,6 +34,51 @@ describe('directions client library', function() {
             })
           ]));
     })
+    .then(done, fail);
+  });
+
+  it('accepts localization options', function(done) {
+    googleMaps.directions({
+      origin: 'Town Hall, Sydney, NSW',
+      destination: 'Parramatta, NSW',
+      language: 'en',
+      units: 'metric',
+      region: 'au',
+    })
+    .asPromise()
+    .then(expectOK)
+    .then(done, fail);
+  });
+
+  it('accepts transit options', function(done) {
+    googleMaps.directions({
+      origin: 'Town Hall, Sydney, NSW',
+      destination: 'Parramatta, NSW',
+      arrival_time: inOneHour,
+      mode: 'transit',
+      alternatives: true,
+      transit_mode: ['bus', 'rail'],
+      transit_routing_preference: 'fewer_transfers',
+    })
+    .asPromise()
+    .then(expectOK)
+    .then(done, fail);
+  });
+
+  it('accepts driving options', function(done) {
+    googleMaps.directions({
+      origin: 'Town Hall, Sydney, NSW',
+      destination: 'Parramatta, NSW',
+      departure_time: inOneHour,
+      mode: 'driving',
+      waypoints: 'Strathfield, NSW',
+      alternatives: true,
+      avoid: ['tolls', 'ferries'],
+      traffic_model: 'best_guess',
+      optimize: true,
+    })
+    .asPromise()
+    .then(expectOK)
     .then(done, fail);
   });
 
