8b31e12183f0fe9c6500ced32cadcc2113350a2c
Stephen Farrar <stephen.farrar@gmail.com>
2015-12-07 10:44:03 +1100

    Add more e2e tests for timezone API. The timestamp is required by the server, but optional in JS, so test that.

--- e2e/timezone-spec.js
+++ e2e/timezone-spec.js
@@ -7,12 +7,31 @@ describe('timezone client library', function() {
     googleMaps.timezone({
       location: [-33.8571965, 151.2151398],
       timestamp: 1331766000,
+      language: 'en'
     })
     .asPromise()
     .then(function(response) {
       expect(response.json).toEqual(
           objectContaining({
-            timeZoneId : 'Australia/Sydney'
+            status: 'OK',
+            rawOffset: 36000,
+            dstOffset: 0,
+            timeZoneId: 'Australia/Sydney'
+          }));
+    })
+    .then(done, fail);
+  });
+
+  it('the timestamp is optional', function(done) {
+    googleMaps.timezone({
+      location: [-33.8571965, 151.2151398]
+    })
+    .asPromise()
+    .then(function(response) {
+      expect(response.json).toEqual(
+          objectContaining({
+            status: 'OK',
+            timeZoneId: 'Australia/Sydney'
           }));
     })
     .then(done, fail);
