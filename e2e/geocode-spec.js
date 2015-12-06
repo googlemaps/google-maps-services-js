b32e555cc5dafc761f3e75d7384bc33bf82079d9
Stephen Farrar <stephen.farrar@gmail.com>
2015-12-07 10:11:45 +1100

    Add e2e tests for geocoding API.

--- e2e/geocode-spec.js
+++ e2e/geocode-spec.js
@@ -1,15 +1,16 @@
 var arrayContaining = jasmine.arrayContaining;
 var objectContaining = jasmine.objectContaining;
+var stringMatching = jasmine.stringMatching;
+
+var googleMaps = require('./service');
+
+function expectOK(response) {
+  expect(response.status).toBe(200);
+  expect(response.json.status).toBe('OK');
+  return response;
+}
 
 describe('geocode client library', function() {
-  var googleMaps = require('./service');
-
-  function expectOK(response) {
-    expect(response.status).toBe(200);
-    expect(response.json.status).toBe('OK');
-    return response;
-  }
-
   function expectOperaHouse(response) {
     expect(response.json.results)
     .toEqual(arrayContaining([
@@ -69,6 +70,17 @@ describe('geocode client library', function() {
     .then(expectOK)
     .then(done, fail);
   });
+});
+
+describe('reverse geocode client library', function() {
+  function expect2aMacquarieStreet(response) {
+    expect(response.json.results)
+    .toEqual(arrayContaining([
+      objectContaining({
+        formatted_address: stringMatching(/2A Macquarie St, Sydney NSW 2000/)
+      })
+    ]));
+  }
 
   it('reverse geocodes the coordinates for the Sydney Opera House', function(done) {
     googleMaps.reverseGeocode({
@@ -76,14 +88,42 @@ describe('geocode client library', function() {
     })
     .asPromise()
     .then(expectOK)
-    .then(function(response) {
-      expect(response.json.results)
-      .toEqual(arrayContaining([
-        objectContaining({
-          formatted_address: '2A Macquarie St, Sydney NSW 2000, Australia'
-        })
-      ]));
-    })
+    .then(expect2aMacquarieStreet)
     .then(done, fail);
   });
+
+  it('reverse geocodes the place ID for the Sydney Opera House', function(done) {
+    googleMaps.reverseGeocode({
+      place_id: 'ChIJidzEjmauEmsRwb535u6rCA4'
+    })
+    .asPromise()
+    .then(expectOK)
+    .then(expect2aMacquarieStreet)
+    .then(done, fail);
+  });
+
+  it('accepts localization options', function(done) {
+    googleMaps.reverseGeocode({
+      place_id: 'ChIJidzEjmauEmsRwb535u6rCA4',
+      language: 'en'
+    })
+    .asPromise()
+    .then(expectOK)
+    .then(expect2aMacquarieStreet)
+    .then(done, fail);
+  });
+
+  it('filters by result_type and location_type', function(done) {
+    pending('does not work with my API key');
+    googleMaps.reverseGeocode({
+      place_id: 'ChIJidzEjmauEmsRwb535u6rCA4',
+      result_type: ['country', 'street_address'],
+      location_type: ['ROOFTOP', 'APPROXIMATE']
+    })
+    .asPromise()
+    .then(expectOK)
+    .then(expect2aMacquarieStreet)
+    .then(done, fail);
+  });
+
 });
