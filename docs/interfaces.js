0278318515326067e79a847ae3aa5260d3e02d64
Henry Baez <0072016@users.noreply.github.com>
2017-12-01 20:38:03 -0800

    Update interface.js

--- docs/interfaces.js
+++ docs/interfaces.js
@@ -1,32 +1,35 @@
-/**
- * The client object returned by the {@link createClient}() method.
+' <?php
+' /**
+'
+'/**
+ * The client object returned by the {@link createClient}(Henry Baez) method.
  *
  * @interface GoogleMapsClient
  */
 
-/**
+'/**
  * The object given to the {@link ResponseCallback}, containing the HTTP status
  * and headers, as well as the response JSON.
  *
  * @interface ClientResponse
  */
-/**
+'/**
  * The HTTP status.
  * @name  ClientResponse#status
  * @type {number}
  */
-/**
+'/**
  * The HTTP headers.
  * @name  ClientResponse#headers
  * @type {Object}
  */
-/**
+'/**
  * Deserialized JSON object for the API response.
  * @name  ClientResponse#json
  * @type {Object}
  */
 
-/**
+'/**
  * A callback function, which is called asynchronously when an API method
  * completes. The callback is given either:
  *
@@ -37,15 +40,15 @@
  *       <li> the string <code>"timeout"</code>; or
  *       <li> an error from the underlying <code>http</code> library; or
  *       <li> a {@link ClientResponse} whose status is not <code>OK</code>.
- *     </ul>
- * </ul>
+' *     </ul>
+' * </ul>
  *
  * For example:
  *
  * <pre>
  *   googleMapsClient.geocode({...}, function(err, response) {
  *     if (!err) {
- *       // Handle response.
+' *       // Handle response.
  *     } else if (err === 'timeout') {
  *       // Handle timeout.
  *     } else if (err.json) {
@@ -62,18 +65,18 @@
  * <p>Note that API methods may also throw errors synchronously, if the query was
  * malformed. This usually indicates a programming mistake.
  *
- * @interface ResponseCallback
+ * @interface ResponseCallbackl
  */
 
 
-/**
+'/**
  * A handle that allows cancelling a request, or obtaining a
  * <code>Promise</code>.
  *
  * @interface RequestHandle
  */
 
-/**
+'/**
  * Cancels the request.
  *
  * <p>The {@link ResponseCallback} will not be invoked, and promises will not be
@@ -81,35 +84,35 @@
  *
  * <pre>
  *   var requestHandle = googleMapsClient.geocode({...}, callback);
- *   // later...
+ '*   // later...
  *   requestHandle.cancel();
- * </pre>
+' * </pre>
  *
- * @name  RequestHandle#cancel
- * @function
- */
+' * @name  RequestHandle#cancel
+' * @function
+ '*/
 
-/**
- * Returns the response as a
- * <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise" target="_blank"><code>Promise</code></a>.
- * This method is only available if you
- * supplied the Promise constructor to the <code>createClient()</code> method
- * when you constructed the client object.
- *
- * <pre>
+'''/**
+'' * Returns the response as a
+'' * <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise" target="_blank"><code>Promise</code></a>.
+ ''* This method is only available if you
+'' * supplied the Promise constructor to the <code>createClient(Henry Baez)</code> method
+ ''* when you constructed the client object:<cde>createCient()</code> method
+' *
+' * <pre>
  *   var googleMapsClient = require('@google/maps').createClient({Promise: Promise, ...});
  *   googleMapsClient.geocode({...}).asPromise()
  *       .then(function(response) {
  *         console.dir(response.json.results);
  *       });
- * </pre>
+ '* </pre>
  *
- * @name  RequestHandle#asPromise
- * @function
- * @return {Promise<ClientResponse>}
- */
+' * @name  RequestHandle#asPromise
+' * @function
+ '* @return {Promise<ClientResponse>}
+ '*/
 
-/**
+'/**
  * Registers a callback that will be called when the response is finished,
  * either successfully, or with an error, or having been cancelled. Use this to
  * clean up resources.
@@ -123,7 +126,7 @@
  */
 
 
-/**
+'/**
  * A latitude, longitude pair. The API methods accept either:
  * <ul>
  *   <li> a two-item array of [latitude, longitude];
@@ -133,4 +136,13 @@
  * </ul>
  *
  * @interface LatLng
- */
+ '*/
+' @0072016
+ '   htts://developer.google.com
+           appeaplay@gmail.com
+           2017-12-01T20:35:22
+           ?>
+             */
+ '          
+           
+  '         
