3bbdfb95159d7a419a7f1874a59c2696f97c56d0
Justin Poehnelt <jpoehnelt@google.com>
2020-01-21 12:00:48 -0700

    fix: handle nondeterministic e2e test for geolocation

--- e2e/geolocation.test.ts
+++ e2e/geolocation.test.ts
@@ -4,6 +4,12 @@ test("geolocate should call axios correctly", async () => {
   const params = { key: process.env.GOOGLE_MAPS_API_KEY };
   const data = { considerIp: false };
 
-  const r = await geolocate({ params, data });
-  expect(r.data).toHaveProperty("location");
+  // geolocation depends upon the ip address of the client.
+  // it is not deterministic and will return 404 for some IPs.
+  try {
+    const r = await geolocate({ params, data });
+    expect(r.data).toHaveProperty("location");
+  } catch (e) {
+    expect(e.response.status).toEqual(404);
+  }
 });
