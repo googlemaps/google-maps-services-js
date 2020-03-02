3bc05b839b9a5e72e6b78f0cd63d6014ba3e183f
Justin Poehnelt <jpoehnelt@google.com>
2020-03-02 12:11:11 -0800

    test: add e2e reversegeocode test with place_id

--- e2e/geocode/reversegeocode.test.ts
+++ e2e/geocode/reversegeocode.test.ts
@@ -11,3 +11,12 @@ test("reverseGeocode should return correct response", async () => {
   const r = await reverseGeocode({ params: params });
   expect(r.data.results.length).toBeTruthy();
 });
+
+test("reverseGeocode should return correct response using place_id", async () => {
+  const params = {
+    place_id: "ChIJKxDbe_lYwokRVf__s8CPn-o",
+    key: process.env.GOOGLE_MAPS_API_KEY
+  };
+  const r = await reverseGeocode({ params: params });
+  expect(r.data.results.length).toBeTruthy();
+});
