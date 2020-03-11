f48d276530d0d2897281b263ca71d6202dae6179
Justin Poehnelt <jpoehnelt@google.com>
2020-03-11 08:47:56 -0700

    feat: add support for elevation along path

--- src/elevation.test.ts
+++ src/elevation.test.ts
@@ -9,8 +9,29 @@ afterEach(() => {
   jest.clearAllMocks();
 });
 
-test("elevation should call axios correctly", () => {
+test("elevation should call axios correctly with location params", () => {
   const params = { locations: ["10,20"], key: "foo" };
+
+  elevation({ params: params }, mockedAxios);
+
+  expect(mockedAxios).toHaveBeenCalledTimes(1);
+  expect(mockedAxios).toHaveBeenCalledWith({
+    method: "get",
+    params: params,
+    paramsSerializer: defaultParamsSerializer,
+    url: defaultUrl
+  });
+});
+
+test("elevation should call axios correctly with path params", () => {
+  const params = {
+    path: [
+      { lat: 35, lng: -110 },
+      { lat: 45, lng: -110 }
+    ],
+    samples: 10,
+    key: "foo"
+  };
 
   elevation({ params: params }, mockedAxios);
 
