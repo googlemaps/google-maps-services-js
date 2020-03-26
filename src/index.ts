10106d304a167b4b4cc16a86ad46455a846eaf99
Justin <jpoehnelt@google.com>
2020-03-26 13:27:47 -0700

    fix: export types at root

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,39 @@
 // Cannot be `import` as it's not under TS root dir
 export const version = require("../package.json").version;
 
+export { DirectionsRequest, DirectionsResponse } from "./directions";
+export { DistanceMatrixRequest, DistanceMatrixResponse } from "./distance";
+export { ElevationRequest, ElevationResponse } from "./elevation";
+export {
+  FindPlaceFromTextRequest,
+  FindPlaceFromTextResponse
+} from "./places/findplacefromtext";
+export { GeocodeRequest, GeocodeResponse } from "./geocode/geocode";
+export { GeolocateRequest, GeolocateResponse } from "./geolocate";
+export {
+  NearestRoadsRequest,
+  NearestRoadsResponse
+} from "./roads/nearestroads";
+export {
+  PlaceAutocompleteRequest,
+  PlaceAutocompleteResponse
+} from "./places/autocomplete";
+export { PlaceDetailsRequest, PlaceDetailsResponse } from "./places/details";
+export { PlacePhotoRequest, PlacePhotoResponse } from "./places/photo";
+export {
+  PlaceQueryAutocompleteRequest,
+  PlaceQueryAutocompleteResponse
+} from "./places/queryautocomplete";
+export {
+  PlacesNearbyRequest,
+  PlacesNearbyResponse
+} from "./places/placesnearby";
+export {
+  ReverseGeocodeRequest,
+  ReverseGeocodeResponse
+} from "./geocode/reversegeocode";
+export { SnapToRoadsRequest, SnapToRoadsResponse } from "./roads/snaptoroads";
+export { TextSearchRequest, TextSearchResponse } from "./places/textsearch";
+export { TimeZoneRequest, TimeZoneResponse } from "./timezone";
+
 export * from "./client";
