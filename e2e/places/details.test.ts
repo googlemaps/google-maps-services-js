5fed9ea11bbc95bec5584c8a34502109b50eca65
Sasha Sorokin <sorokin.sh@gmail.com>
2020-06-07 13:58:27 +0400

    fix(doc): typo

--- e2e/places/details.test.ts
+++ e2e/places/details.test.ts
@@ -16,7 +16,7 @@
 
 import { placeDetails } from "../../src/places/details";
 
-test("autocomplete should return correct result", async () => {
+test("details should return correct result", async () => {
   const params = {
     place_id: "ChIJKxDbe_lYwokRVf__s8CPn-o",
     key: process.env.GOOGLE_MAPS_API_KEY,
