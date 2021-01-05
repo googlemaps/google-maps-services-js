472b826dc87f500131b0d5d0781e20211eb91262
Justin Poehnelt <jpoehnelt@google.com>
2021-01-05 16:48:08 +0000

    build: update typedoc settings

--- typedoc.js
+++ typedoc.js
@@ -15,12 +15,8 @@
  */
 
 module.exports = {
-  mode: "modules",
-  target: "es6",
   out: "docs",
   exclude: ["**/node_modules/**", "**/*.spec.ts", "**/*.test.ts", "dist"],
   name: "Google Maps Services Node Client",
-  ignoreCompilerErrors: false,
-  module: "commonjs",
   readme: "./README.md"
 };
