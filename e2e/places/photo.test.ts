b861610d8c465c9d282906ae1493f191ad812341
Alex Muramoto <amuramoto@users.noreply.github.com>
2023-06-15 15:27:46 -0700

    fix: Update photo.test.ts (#996)

--- e2e/places/photo.test.ts
+++ e2e/places/photo.test.ts
@@ -19,7 +19,7 @@ import { placePhoto } from "../../src/places/photo";
 test("photo should return correct result", async () => {
   const params = {
     photoreference:
-      "CnRtAAAATLZNl354RwP_9UKbQ_5Psy40texXePv4oAlgP4qNEkdIrkyse7rPXYGd9D_Uj1rVsQdWT4oRz4QrYAJNpFX7rzqqMlZw2h2E2y5IKMUZ7ouD_SlcHxYq1yL4KbKUv3qtWgTK0A6QbGh87GB3sscrHRIQiG2RrmU_jF4tENr9wGS_YxoUSSDrYjWmrNfeEHSGSc3FyhNLlBU",
+      "AZose0kqzNAxzQSLYC-kxAidG_FkCelqG6lXa-05yhbKfwfGd0Agu4YXSjvArtNEYLC8CiCvQb4uxQzfn2fZi2kzxeMBgtOHmbo0c8eqMx_YPVNRCjjPL_kA77NOWF5wOS2ub6ZQlM0G8XibN93burBky0JLCE5sf-C6gLVEG74yiPYoK8id",
     maxwidth: 100,
     maxheight: 100,
     key: process.env.GOOGLE_MAPS_API_KEY,
