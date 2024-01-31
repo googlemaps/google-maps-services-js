24b4fc7478054db81abc76173903318dd3ab1252
wiredmatt <29718978+wiredmatt@users.noreply.github.com>
2024-01-31 11:14:26 -0300

    chore: add review params

--- dist/places/details.d.ts
+++ dist/places/details.d.ts
@@ -47,6 +47,18 @@ export interface PlaceDetailsRequest extends Partial<AxiosRequestConfig> {
          * This applies only to Place Details requests.
          */
         fields?: string[];
+        /**
+         * The sorting method to use when returning reviews. Can be set to most_relevant (default) or newest.
+         * For most_relevant (default), reviews are sorted by relevance; the service will bias the results to return reviews originally written in the preferred language.
+         * For newest, reviews are sorted in chronological order; the preferred language does not affect the sort order.
+         * Google recommends that you display how the reviews are being sorted to the end user.
+         */
+        reviews_sort?: string;
+        /**
+         * Specify reviews_no_translations=true to disable translation of reviews; specify reviews_no_translations=false to enable translation of reviews. Reviews are returned in their original language.
+         * If omitted, or passed with no value, translation of reviews is enabled. If the language parameter was specified in the request, use the specified language as the preferred language for translation. If language is omitted, the API attempts to use the Accept-Language header as the preferred language.
+         */
+        reviews_no_translations?: boolean;
     } & RequestParams;
 }
 export interface PlaceDetailsResponseData extends ResponseData {
